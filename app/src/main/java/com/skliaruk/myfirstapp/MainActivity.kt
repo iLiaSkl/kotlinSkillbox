8ef6b93f5ba18550ff7fa2e8abcc1eab8a18c0df
Ilia Skliaruk <ilia.skliaruk2@edu.lapinamk.fi>
2020-02-11 15:13:10 +0200

    2_14

---
+++ app/src/main/java/com/skliaruk/myfirstapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.skliaruk.myfirstapp
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = """
+        BuildType=${BuildConfig.BUILD_TYPE}
+        Flavor=${BuildConfig.FLAVOR}
+        VersionCode=${BuildConfig.VERSION_CODE}
+        VersionName=${BuildConfig.VERSION_NAME}
+        ApplicationID=${BuildConfig.APPLICATION_ID}
+            
+        """
+    }
+}
