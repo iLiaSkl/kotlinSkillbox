9b43cc0743cf8a5efa84860cda0061d4a982c5a3
Ilia Skliaruk <ilia.skliaruk2@edu.lapinamk.fi>
2020-02-20 20:45:34 +0200

    Some fixes

--- app/src/main/java/module_3/NOD.kt
+++ app/src/main/java/module_3/NOD.kt
@@ -3,56 +3,50 @@ package module_3
 import kotlin.math.abs
 
 fun main(){
+
     println("Введите количество чисел: ")
-    val inputNumber = readLine()?.toIntOrNull()
-    inputNumber?.let { getNumberFromConsole(it) }
+    val inputNumber = readLine()?.toIntOrNull() ?: return
+    val numbersList =  getNumbersFromConsole(inputNumber)
+    val positiveNumbers = mutableListOf<Int>()
+
+    numbersList.forEach { if (it > 0){
+        positiveNumbers.add(it) } }
+
+    println("Positive numbers: ${positiveNumbers.size}")
+
+    println(numbersList.filter { it % 2 == 0 })
+
+    numbersList.sum()
+
+    println("Unique numbers: ${numbersList.toSet().size}")
+
+
+    numbersList.forEach {
+        val nodNumber = findGreatestCommonDividerValue(it, numbersList.toSet().sum())
+
+        mapOf(
+            it to nodNumber
+        )
+
+        println("Int $it, sum ${numbersList.toSet().sum()}, NOD $nodNumber")
+    }
 
 }
 
-fun getNumberFromConsole(inputNumber: Int): MutableList<Int>  {
-    val numbersList = mutableListOf<Int>()
-    val positiveNumbers = mutableListOf<Int>()
+fun getNumbersFromConsole(inputNumber: Int): MutableList<Int>  {
 
+    val numbersList = mutableListOf<Int>()
     for (item in 1..inputNumber) {
         println("Введите $item число:")
         val number = readLine()?.toIntOrNull() ?: continue
         numbersList.add(number)
-        if (number > 0){
-            positiveNumbers.add(number)
-        }
     }
-
-    val evenNumbers = numbersList.filter { it % 2 == 0 }
-    val allNumbersSum = numbersList.sum()
-    val nodNumbersList = mutableListOf<Int>()
-    val numbersSet = numbersList.toSet()
-    val uniqueNumbersCount = numbersSet.distinct()
-
-    numbersList.forEach {
-        val nodNumber = findGCDValue(it, allNumbersSum)
-
-        nodNumbersList.add(nodNumber)
-
-        val map = mapOf(
-            it to nodNumber
-        )
-        println(map)
-        println("Int $it, sum $allNumbersSum, NOD $nodNumber")
-    }
-
-    println("All numbers $numbersList")
-    println("Positive numbers $positiveNumbers")
-    println("Unique numbers: $uniqueNumbersCount")
-    println("Even numbers $evenNumbers")
-    println("Sum: $allNumbersSum")
-
-
     return numbersList
 }
 
-tailrec fun findGCDValue(n1: Int, n2: Int): Int =
+tailrec fun findGreatestCommonDividerValue(n1: Int, n2: Int): Int =
     when {
         n1 == n2 -> n1
-        n1 > n2 -> findGCDValue(n1 - n2, n2)
-        else -> findGCDValue(abs(n1), n2-n1)
+        n1 > n2 -> findGreatestCommonDividerValue(n1 - n2, n2)
+        else -> findGreatestCommonDividerValue(abs(n1), n2-n1)
     }
